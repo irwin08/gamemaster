4b6aba9c90b8a119e253f150356618fb2277b6b1
Jesse <jesse.irwin11@gmail.com>
2021-10-12 14:01:51 -0700

    test case test again

--- test/main_test.rb
+++ test/main_test.rb
@@ -1,6 +1,8 @@
 require_relative '../main'
 require 'test/unit'
 
-class MainTest << Test::Unit::TestCase
-    assert_equal('hi', method_1)
+class MainTest < Test::Unit::TestCase
+    def method_1_test
+        assert_equal('hi', method_1)
+    end
 end
