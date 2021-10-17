bb739ef93e33f7a5c66e89781cd0dff5548e13dd
Irwin08 <jesse.irwin11@gmail.com>
2021-10-17 15:27:55 -0700

    hello world example working

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 
 require 'discordrb'
 
-bot = Discordrb::Bot.new token: 'BOT TOKEN HERE'
+bot = Discordrb::Bot.new token: ENV['GAMEMASTER_TOKEN']
 
 bot.message(contains: 'Where to?') do |event|
     event.respond "The library, and step on it."
