35dc65faacb14b67b899b05962110bb7d358bdc3
Jesse <jesse.irwin11@gmail.com>
2021-10-25 20:08:04 -0700

    fixing workflow env variable

---
+++ game.rb
@@ -0,0 +1,12 @@
+require 'date'
+
+class Game
+    def initialize(name, game_datetime)
+        @name = name
+        @date = game_datetime
+        @players = []
+    end
+
+    def add_player
+    end
+end
